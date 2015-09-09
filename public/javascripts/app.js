9e18dff41a134c20f6ce58178bcdcb68c673e1f2
Zack Claar <claar.zack@gmail.com>
2015-09-09 15:14:54 -0600

    Have partial coming up for the individual movie imdb query

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,5 +5,8 @@ app.config(function ($routeProvider) {
     .when('/', {
       templateUrl: "partials/moviesQuery.html",
       controller: "SearchController"
+    }).when('/:id', {
+      templateUrl: 'partials/titleQuery.html',
+      controller: "SearchController"
     });
 });
