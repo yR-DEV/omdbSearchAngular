631f02c56e0a4fd5f60c689e1620460cd05eb6c5
Zack Claar <claar.zack@gmail.com>
2015-09-09 11:32:29 -0600

    Still getting this error :modulerr thing. I may just burn it down and restart when i get home

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,30 +1,29 @@
-app.controller("SearchController", [$scope, $http,
-  function($scope, $http) {
-    $scope.searchMovieTitle = function(title) {
-      $http.get('http://www.omdbapi.com/?s=' + title)
-        .then(function(data) {
-          console.log(data);
-      })
-    }
-}]);
-
 // app.controller("SearchController", function($scope, $http) {
-//   $scope.bool = false;
-//   $scope.showResultsBool = false;
-//
 //   $scope.searchMovieTitle = function(title) {
-//     // console.log('in search');
 //     $http.get('http://www.omdbapi.com/?s=' + title)
-//       .then(function(data) {
-//         console.log(data);
-//         return $scope.searchResult = data.data.search;
-//       }, function(data) {
-//         return console.log('not data');
-//       });
-//     $scope.showResultsBool = true;
-//   };
-//
+//     .then(function(data) {
+//       console.log(data);
+//     })
+//   }
 // });
+
+app.controller("SearchController", function($scope, $http) {
+  $scope.bool = false;
+  $scope.showResultsBool = false;
+
+  $scope.searchMovieTitle = function(title) {
+    // console.log('in search');
+    $http.get('http://www.omdbapi.com/?s=' + title)
+      .then(function(data) {
+        console.log(data);
+        return $scope.searchResult = data.data.search;
+      }, function(data) {
+        return console.log('not data');
+      });
+    $scope.showResultsBool = true;
+  };
+
+});
 //
 // //need to figure out a way to get the data quereyed from the API from that
 // app.controller("TitleQueryController", function($scope, $http) {
